1cf1845622e693103bc9107557c21a46739f56a3
mattb0t <matthew.ballou@gmail.com>
2016-10-20 08:49:18 -0400

    Added getAverage function.

--- Transcript.java
+++ Transcript.java
@@ -10,28 +10,47 @@ public class Transcript {
   Integer YearofGraduation,
   Float GPA,
   String Address,
-  HashMap<String, List<String>> Courses,
+  HashMap<String, Float> Courses,
   Integer StudentID) {
 
     String name = Name;
     Integer age = Age;
     Integer yog = YearofGraduation;
     Float gpa = GPA;
-    HashMap<String, List<String>> courses = Courses;
+    HashMap<String, Float> courses = Courses;
     Integer studentid = StudentID;
+
+  }
+
+  public float averageGPA(HashMap<String, Float> Courses) {
+
+    int x = 0;
+    Float average = 0.0f;
+
+    HashMap<String, Float> courses = Courses;
+
+    for(String course : courses.keySet()) {
+
+      average = average + courses.get(course);
+      x++;
+
+    }
+
+    average = average / x;
+
+    return average;
 
   }
 
   public static void main(String[] args) {
 
-    HashMap<String, List<String>> courses = new HashMap<String, List<String>>();
-    List<String> values = new ArrayList<String>();
+    HashMap<String, Float> courses = new HashMap<String, Float>();
 
     Transcript matthew = new Transcript(
     "Matthew Ballou",
     16,
     2018,
-    4.0f,
+    4.0f, //eventually this will be changed to read the courses of the student
     "101 Fake Street",
     courses,
     1858067
