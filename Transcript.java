ec1e13f544f809fc2c8a1a00569bd67298c1851f
mattb0t <matthew.ballou@gmail.com>
2016-10-21 08:33:25 -0400

    getCourses() and Print Statements Added

--- Transcript.java
+++ Transcript.java
@@ -5,24 +5,42 @@ import java.util.List;
 
 public class Transcript {
 
-  public Transcript(String Name,
-  Integer Age,
-  Integer YearofGraduation,
-  Float GPA,
-  String Address,
-  HashMap<String, Float> Courses,
-  Integer StudentID) {
+  String name;
+  Integer age;
+  Integer yog;
+  Float gpa;
+  HashMap<String, Float> courses;
+  Integer studentid;
+
+  public Transcript(
+    String Name,
+    Integer Age,
+    Integer YearofGraduation,
+    Float GPA,
+    String Address,
+    HashMap<String, Float> Courses,
+    Integer StudentID
+  ) {
 
     String name = Name;
-    Integer age = Age;
-    Integer yog = YearofGraduation;
-    Float gpa = GPA;
-    HashMap<String, Float> courses = Courses;
-    Integer studentid = StudentID;
+    age = Age;
+    yog = YearofGraduation;
+    gpa = GPA;
+    courses = Courses;
+    studentid = StudentID;
 
   }
 
-  public float averageGPA(HashMap<String, Float> Courses) {
+  public static void getCourses(HashMap<String, Float> Courses) {
+    HashMap<String, Float> courses = Courses;
+
+    for(String course : courses.keySet()) {
+      System.out.println(course + ": " + courses.get(course));
+    }
+
+  }
+
+  public static float averageGPA(HashMap<String, Float> Courses) {
 
     int x = 0;
     Float average = 0.0f;
@@ -38,6 +56,8 @@ public class Transcript {
 
     average = average / x;
 
+    average = (float) Math.floor((double)average) + 1.0f;
+
     return average;
 
   }
@@ -46,16 +66,29 @@ public class Transcript {
 
     HashMap<String, Float> courses = new HashMap<String, Float>();
 
-    Transcript matthew = new Transcript(
-    "Matthew Ballou",
-    16,
-    2018,
-    4.0f, //eventually this will be changed to read the courses of the student
-    "101 Fake Street",
-    courses,
-    1858067
+    Transcript matt = new Transcript(
+      "Matthew Ballou",
+      16,
+      2018,
+      4.0f,
+      "101 Fake Street",
+      courses,
+      1858067
     );
 
+    courses.put("Programming II",       96.0f);
+    courses.put("Chemistry",            98.0f);
+    courses.put("U.S. History",         88.0f);
+    courses.put("English Composition",  92.0f);
+    courses.put("Pre-Calculus",         98.0f);
+    courses.put("Spanish III",          91.0f);
+
+    System.out.println(matt.name);
+    //System.out.println(matt.age);
+    //System.out.println(matt.yog);
+    System.out.println(averageGPA(courses));
+    //System.out.println(getCourses());
+    //System.out.println(matt.studentid);
 
   }
 
