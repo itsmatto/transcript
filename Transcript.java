8f68803fa8e6a62b95f43e2d6afcac1b89a43fd4
mattb0t <matthew.ballou@gmail.com>
2016-10-25 08:45:35 -0400

    Write to CSV completed

--- Transcript.java
+++ Transcript.java
@@ -2,6 +2,10 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
 
 public class Transcript {
 
@@ -22,21 +26,12 @@ public class Transcript {
     Integer StudentID
   ) {
 
-    String name = Name;
+    name = Name;
     age = Age;
     yog = YearofGraduation;
     gpa = GPA;
     courses = Courses;
     studentid = StudentID;
-
-  }
-
-  public static void getCourses(HashMap<String, Float> Courses) {
-    HashMap<String, Float> courses = Courses;
-
-    for(String course : courses.keySet()) {
-      System.out.println(course + ": " + courses.get(course));
-    }
 
   }
 
@@ -49,16 +44,57 @@ public class Transcript {
 
     for(String course : courses.keySet()) {
 
-      average = average + courses.get(course);
+      average = (average + (float)courses.get(course));
       x++;
 
     }
 
     average = average / x;
-
-    average = (float) Math.floor((double)average) + 1.0f;
+    average = average / 25;
 
     return average;
+
+  }
+
+  public static void makeTranscript(HashMap<String, Float> Courses, Transcript Student) throws FileNotFoundException {
+
+    PrintWriter writer = new PrintWriter(new File("transcript.csv"));
+    StringBuilder sb = new StringBuilder();
+
+    HashMap<String, Float> courses = Courses;
+    Transcript student = Student;
+
+    sb.append("Name");
+    sb.append(',');
+    sb.append("Age");
+    sb.append(',');
+    sb.append("Year of Graduation");
+    sb.append(',');
+    sb.append("GPA");
+    sb.append(',');
+    sb.append("Student ID");
+    sb.append('\n');
+
+    sb.append(student.name);
+    sb.append(',');
+    sb.append(student.age);
+    sb.append(',');
+    sb.append(student.yog);
+    sb.append(',');
+    sb.append(averageGPA(student.courses));
+    sb.append(',');
+    sb.append(student.studentid);
+    sb.append('\n');
+
+    for(String course : student.courses.keySet()) {
+      sb.append(course);
+      sb.append(",");
+      sb.append(student.courses.get(course));
+      sb.append('\n');
+    }
+
+    writer.write(sb.toString());
+    writer.close();
 
   }
 
@@ -70,7 +106,7 @@ public class Transcript {
       "Matthew Ballou",
       16,
       2018,
-      4.0f,
+      averageGPA(courses),
       "101 Fake Street",
       courses,
       1858067
@@ -78,17 +114,29 @@ public class Transcript {
 
     courses.put("Programming II",       96.0f);
     courses.put("Chemistry",            98.0f);
-    courses.put("U.S. History",         88.0f);
+    courses.put("U.S. History",         92.0f);
     courses.put("English Composition",  92.0f);
     courses.put("Pre-Calculus",         98.0f);
     courses.put("Spanish III",          91.0f);
 
     System.out.println(matt.name);
-    //System.out.println(matt.age);
-    //System.out.println(matt.yog);
+    System.out.println(matt.age);
+    System.out.println(matt.yog);
     System.out.println(averageGPA(courses));
-    //System.out.println(getCourses());
-    //System.out.println(matt.studentid);
+    System.out.println(matt.studentid);
+
+    for(String course : courses.keySet()) {
+      System.out.println(course + ": " + courses.get(course));
+    }
+
+    try {
+      makeTranscript(courses, matt);
+      System.out.println("Transcript successfully made in this directory.");
+    }
+    catch (Exception e) {
+      System.out.println("Transcript has not been made or updated. See the error below.");
+      System.out.println(e);
+    }
 
   }
 
